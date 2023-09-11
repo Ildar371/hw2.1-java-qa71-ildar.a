4981a3c60ebd234f8982eae672df4ea170817067
ildar.a <ildar371@ya.ru>
2023-09-11 12:59:40 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 100;
+        int bonusSize = ticketPrice / 20;
+
+        System.out.println("Начисленные мили: " + bonusSize);
+    }
+}
